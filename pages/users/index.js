2fd64889b20e2c1ab8383d1b46f1117c947744ca
erisondasilva <55626910+erisondasilva@users.noreply.github.com>
2025-07-02 14:36:57 -0300

    Add EditorFilter component to UsersEditor for filtering functionality

--- pages/users/index.js
+++ pages/users/index.js
@@ -5,6 +5,7 @@ import DashboardNavbar from "shared-ui/layout/DashboardNavbar";
 import Footer from "shared-ui/layout/Footer";
 import EditorController from "shared-ui/components/editor/EditorController";
 import { userConfig } from "./user-config"
+import EditorFilter from 'shared-ui/components/editor/EditorFilter';
 
 function UsersEditor() {
     return (
@@ -12,6 +13,19 @@ function UsersEditor() {
         <DashboardNavbar absolute={true}/>
             <EditorController
                 registerConfig={userConfig}
+
+                defaultFilterData={{ active: true }}
+                renderFilter={
+                    ({ filterData, setFilterData }) => {
+                    return (
+                        <EditorFilter
+                            filterData={filterData}
+                            setFilterData={setFilterData} 
+                            fields={[
+                                { label: "Ativos", name: "active", default: true, type: "boolean" },
+                            ]}/>
+                    );
+                }}
             />
         <Footer/>
     </DashboardLayout>
